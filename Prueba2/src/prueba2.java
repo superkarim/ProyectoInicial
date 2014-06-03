8d41c142c4369e7ef4cf32c0f2ed846f08c7117b
karim <chaachoukarim@gmail.com>
2014-06-03 20:04:50 +0200

    segundo cambio

--- Prueba2/src/prueba2.java
+++ Prueba2/src/prueba2.java
@@ -4,6 +4,8 @@ public class prueba2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hola buenas este es el primer cambio");
+System.out.println(" segundo cambio");
+
 	}
 
 }
