c414578064b2e27bf3ef09f7fddec4e354358ee1
karim <chaachoukarim@gmail.com>
2014-06-03 19:58:16 +0200

    este es el primer cambio de la segunda parte

---
+++ Prueba2/src/prueba2.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class prueba2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hola buenas este es el primer cambio");
+	}
+
+}
