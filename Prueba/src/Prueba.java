4702b7c3a23892f9cb1ddd861179eb4d19d99b9d
karim <chaachoukarim@gmail.com>
2014-06-03 18:24:22 +0200

    ksfgnsdf

--- Prueba/src/Prueba.java
+++ Prueba/src/Prueba.java
@@ -6,6 +6,8 @@ public class Prueba {
 			System.out.println("Esto es una prueba");
 			System.out.println("Esto es el segundo paso de la prueba");
 			System.out.println(" ");
+			
+			System.out.println("         ");
 	}
 
 }
