0e029bb8173891c679643b88648108c4c40c4777
karim <chaachoukarim@gmail.com>
2014-06-03 21:06:05 +0200

    1233444

--- Karim/src/Karim.java
+++ Karim/src/Karim.java
@@ -4,6 +4,7 @@ public class Karim {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Esto es un comentario ");
+System.out.println("Este es el segundo comentario que pongo en el dia de hoy");
 	}
 
 }
